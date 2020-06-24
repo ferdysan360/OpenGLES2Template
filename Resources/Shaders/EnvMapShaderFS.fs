381f51e0f72ec6c3e9f6dfe4a7d82502d26fe499
Ferdy <13517116@std.stei.itb.ac.id>
2020-06-24 15:27:44 +0700

    - Added diffuse lighting

--- Resources/Shaders/EnvMapShaderFS.fs
+++ Resources/Shaders/EnvMapShaderFS.fs
@@ -5,7 +5,11 @@ varying vec3 v_pos;
 varying vec3 v_norm;
 void main()
 {
+	float ambientStrength = 0.1;
+	vec3 lightColor = vec3(1.0, 0.0, 0.0);
+	vec3 ambient = ambientStrength * lightColor;
+
 	vec3 toEye = u_camPosition - v_pos;
-	vec3 reflectDir = reflect(normalize(-toEye), normalize(v_norm)); // reflect() in non linear
+	vec3 reflectDir = reflect(normalize(-toEye), normalize(v_norm));
 	gl_FragColor = textureCube(u_texture, reflectDir);
 }
